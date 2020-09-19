9d51e7e7f906dc4593f3a016d358a7d877f0eee7
parikshit <parikshit@gamil.com>
2020-09-19 12:18:15 +0530

    fashionmnist

---
+++ cifar10_resnet_first_set/stn_trainer.py
@@ -0,0 +1,73 @@
+from __future__ import print_function
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+from torchvision import datasets, transforms
+import numpy as np
+from model import STN
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = STN().to(device)
+optimizer = optim.SGD(model.parameters(), lr=0.01)
+
+dataDir = '../data/'
+checkpointDir = '../checkpoints/'
+
+# Training dataset
+train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST(root=dataDir, train=True, download=True,
+        transform=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+        ])), batch_size=64, shuffle=True, num_workers=4)
+# Test dataset
+test_loader = torch.utils.data.DataLoader(
+    datasets.MNIST(root=dataDir, train=False, transform=transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])), batch_size=64, shuffle=True, num_workers=4)
+
+
+def train(epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 500 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+
+
+def test():
+    with torch.no_grad():
+        model.eval()
+        test_loss = 0
+        correct = 0
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            
+            # sum up batch loss
+            test_loss += F.nll_loss(output, target, size_average=False).item()
+            # get the index of the max log-probability
+            pred = output.max(1, keepdim=True)[1]
+            correct += pred.eq(target.view_as(pred)).sum().item()
+            
+            test_loss /= len(test_loader.dataset)
+            print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'
+              .format(test_loss, correct, len(test_loader.dataset),
+                      100. * correct / len(test_loader.dataset)))
+
+for epoch in range(1, 20 + 1):
+    train(epoch)
+    test()
+torch.save(model.state_dict(), os.path.join(checkpointDir,'stn.pth'))
--- mnist/model.py
+++ mnist/model.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 
+
 class BasicBlock(nn.Module):
     expansion = 1
 
@@ -94,29 +95,97 @@ class ResNet(nn.Module):
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
-        
+
+            
 def ResNet18():
     return ResNet(BasicBlock, [2, 2, 2, 2])
 
-def ResNet34():
-    return ResNet(BasicBlock, [3, 4, 6, 3])
+
+class ConvNN(nn.Module):
+    def __init__(self, num_classes=10):
+        super(ConvNN, self).__init__()
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(64, 32, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.linear = nn.Linear(32*32*32, num_classes)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = out.flatten(start_dim=1)
+        out = self.linear(out)
+        return out
 
 
-def ResNet50():
-    return ResNet(Bottleneck, [3, 4, 6, 3])
+class BasicNN(nn.Module):
+    def __init__(self):
+        super(BasicNN, self).__init__()
+        self.fc1 = nn.Linear(28*28, 256)
+        self.fc2 = nn.Linear(256, 10)
 
+    def forward(self,x):
+        return self.fc2(self.fc1(x.view(x.shape[0],-1)).clamp(min=0)).squeeze()
 
-def ResNet101():
-    return ResNet(Bottleneck, [3, 4, 23, 3])
+class LinearModel(nn.Module):
+    def __init__(self):
+        super(LinearModel, self).__init__()
+        self.fc1 = nn.Linear(28*28, 2)
 
-
-def ResNet152():
-    return ResNet(Bottleneck, [3, 8, 36, 3])
-
-
-def test():
-    net = ResNet18()
-    y = net(torch.randn(1, 3, 32, 32))
-    print(y.size())
-
-# test()
+    def forward(self,x):
+        return self.fc1(x.view(x.shape[0],-1)).squeeze()
+        
+class STN(nn.Module):
+    def __init__(self):
+        super(STN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+        self.localization = nn.Sequential(
+            nn.Conv2d(1, 8, kernel_size=7),
+            nn.MaxPool2d(2, stride=2),
+            nn.ReLU(True),
+            nn.Conv2d(8, 10, kernel_size=5),
+            nn.MaxPool2d(2, stride=2),
+            nn.ReLU(True)
+        )
+        
+        # Regressor for the 3 * 2 affine matrix
+        self.fc_loc = nn.Sequential(
+            nn.Linear(10 * 3 * 3, 32),
+            nn.ReLU(True),
+            nn.Linear(32, 3 * 2)
+        )
+        
+        # Initialize the weights/bias with identity transformation
+        self.fc_loc[2].weight.data.zero_()
+        self.fc_loc[2].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
+        
+        # Spatial transformer network forward function
+    def stn(self, x):
+        xs = self.localization(x)
+        xs = xs.view(-1, 10 * 3 * 3)
+        theta = self.fc_loc(xs)
+        theta = theta.view(-1, 2, 3)
+        grid = F.affine_grid(theta, x.size())
+        x = F.grid_sample(x, grid)
+        return x
+        
+    def forward(self, x):
+        # transform the input
+        x = self.stn(x)
+        
+        # Perform the usual forward pass
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+        
